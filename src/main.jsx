8329906f68d0fd4c8c64e360f6a376cd073ab6e6
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-11 11:25:09 +0000

    feat(resilience): wire app-level error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import AppErrorBoundary from './components/AppErrorBoundary'
 import './styles/index.css'
 import './styles/documents.css'
 import './styles/document-detail.css'
@@ -13,4 +14,12 @@ import './styles/sgi.css'
 import './styles/brand.css'
 import './styles/tenants.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppErrorBoundary>
+  </React.StrictMode>,
+)
