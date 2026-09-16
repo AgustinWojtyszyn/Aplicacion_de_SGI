a94682bc4b823be6c6c3d248fbda66b670a87392
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-16 16:07:11 -0300

    style: load work dashboard styles

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import './styles/sgi.css'
 import './styles/brand.css'
 import './styles/tenants.css'
 import './styles/work-entries.css'
+import './styles/work-dashboard.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
