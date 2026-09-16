c1fdaefaa9963bda2a3576185426dc15a61de8fd
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-16 16:18:41 -0300

    style: load profile styles

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ import './styles/tenants.css'
 import './styles/work-entries.css'
 import './styles/work-dashboard.css'
 import './styles/document-followup.css'
+import './styles/profile.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
