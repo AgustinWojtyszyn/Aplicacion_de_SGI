ae26a882522779b48bdc22262c2d50136335e7fb
Agustin Wojtysyzn <agustinwojtyszyn99@gmail.com>
2026-09-22 18:57:18 -0300

    rebrand src/pages/CompaniesPage.jsx to gestiQa

--- src/pages/CompaniesPage.jsx
+++ src/pages/CompaniesPage.jsx
@@ -383,7 +383,7 @@ export default function CompaniesPage() {
                     disabled={isSystemCompany}
                     required
                   />
-                  {isSystemCompany && <small>El identificador principal de EP Consultora está protegido.</small>}
+                  {isSystemCompany && <small>El identificador principal de gestiQa está protegido.</small>}
                 </label>
               )}
 
